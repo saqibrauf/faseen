8bdcdb07ee6fb7109d057c6762c5a1e739582bff
saqib Rauf <saqibrauf@yahoo.com>
2018-11-11 18:31:33 +0500

    changes

--- circulars/urls.py
+++ circulars/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('stores', views.all_stores, name = 'all_stores'),
     path('store/<slug>', views.store_detail, name = 'store_detail'),
+    path('flyers', views.all_flyers, name = 'all_flyers'),
+    path('flyer/<slug>', views.flyer_detail, name = 'flyer_detail'),
+    path('coupons', views.all_coupons, name = 'all_coupons'),
 ]
--- circulars/views.py
+++ circulars/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Location, Store, Flyer,Coupon, Product, Tag, Category
+from .models import Location, Store, Flyer, Coupon, Product, Tag, Category
 
 
 def index(request):
@@ -13,6 +13,15 @@ def index(request):
 	}
 	return render(request, 'circulars/index.html', context)
 
+
+#Store Related Views#######################################################################
+def all_stores(request):
+	stores = Store.objects.all()
+	context = {
+		'stores' : stores,
+	}
+	return render(request, 'circulars/all_stores.html', context)
+
 def store_detail(request, slug):
 	store = Store.objects.get(slug=slug)
 	coupons = store.coupons.all()
@@ -23,3 +32,29 @@ def store_detail(request, slug):
 		'flyers' : flyers,
 	}
 	return render(request, 'circulars/store_detail.html', context)
+
+
+#Flyers Related Views#######################################################################
+def all_flyers(request):
+	flyers = Flyer.objects.all()
+	context = {
+		'flyers' : flyers,
+	}
+	return render(request, 'circulars/all_flyers.html', context)
+
+def flyer_detail(request, slug):
+	flyer = Flyer.objects.get(slug=slug)
+	products = flyer.products.all()
+	context = {
+		'flyer' : flyer,
+		'products' : products,
+	}
+	return render(request, 'circulars/flyer_detail.html', context)
+
+#Coupon Related Views#######################################################################
+def all_coupons(request):
+	coupons = Coupon.objects.all()
+	context = {
+		'coupons' : coupons,
+	}
+	return render(request, 'circulars/all_coupons.html', context)
